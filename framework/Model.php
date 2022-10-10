3f96d29c017473c2c379d6bd4f1170bf96885ede
Jonathan Drakes <jonathan_drakes@hotmail.com>
2022-10-10 18:02:19 -0400

    Model Update

--- framework/Model.php
+++ framework/Model.php
@@ -3,6 +3,7 @@
 abstract class Model 
 {
 
+    protected $cashed_json = [];
     abstract protected function getAll() : array;
     abstract protected function getRecord(string $id):  array;
  
@@ -37,6 +38,15 @@ class ConcreteModelClass extends Model
         //return single database record 
         echo $json_data[$id];
 
+    }
+
+    public function loadData(string $fromFile) : array
+    {
+        $filename = basename($fromFile, 'json');
+        if (!isset($this->cached_json[$filename]) || empty($this-cached_json[$filename])) {
+            $this->cached_json[$filename] = json_decode($json_file, true);
+        }
+        return $this->cached_json[$filename];
     }
 }
     
